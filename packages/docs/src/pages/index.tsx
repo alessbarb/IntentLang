0e76cd1134b0b6951bb762920dbe86eae55fdd28
alessbarb <alessbarb@gmail.com>
2025-08-24 03:09:53 +0200

    chore(docs): add TS config & ambient types for Docusaurus; fix homepage comment

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { type JSX } from "react";
 import { Redirect } from "@docusaurus/router";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
-/\*_ Redirecta / → /docs/handbook / from - scratch respetando baseUrl y locale._ /
+/** Redirige "/" -> "/docs/handbook/from-scratch" respetando baseUrl y locale. */
 export default function Home(): JSX.Element {
   const to = useBaseUrl("/docs/handbook/from-scratch");
   return <Redirect to={to} />;
